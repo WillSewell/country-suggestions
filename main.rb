d095991f66f2dd5f83f8143b46c78543be781e3e
Will Sewell <me@willsewell.name>
2014-12-28 16:26:50 +0000

    Switched over to using websockets.
    Messages are now sent from the client to serer about the clicked country.

--- main.rb
+++ main.rb
@@ -5,32 +5,29 @@
 require 'rubygems'
 require 'eventmachine'
 require 'em-hiredis'
+require 'em-websocket'
 require 'json'
 
-class EchoServer < EventMachine::Connection
-  def initialize
-    @redis = EM::Hiredis.connect
-  end
+EM::run do
+  redis = EM::Hiredis.connect
+  EM::WebSocket.run(:host => "127.0.0.1", :port => 8081) do |ws|
+    ws.onopen { |handshake|
+      puts "WebSocket connection open"
+    }
 
-  def post_init
-    puts "-- someone connected to the server!"
-  end
+    ws.onclose { puts "Connection closed" }
 
-  def receive_data raw
-    begin
-      data = JSON.parse raw
-      @redis.sadd(data["user"], data["country"]).callback {
-        @redis.smembers(data["user"]).callback { |get_res|
-          send_data ">>> cached: #{get_res}"
+    ws.onmessage { |msg|
+      begin
+        data = JSON.parse msg
+        redis.sadd(data["user"], data["country"]).callback {
+          redis.smembers(data["user"]).callback { |get_res|
+            ws.send ">>> cached: #{get_res}"
+          }
         }
-      }
-    rescue JSON::ParserError
-      send_data "failed to parse JSON!"
-    end
+      rescue JSON::ParserError
+        ws.send "failed to parse JSON!"
+      end
+    }
   end
-end
-
-EventMachine::run do
-  EventMachine::start_server "127.0.0.1", 8081, EchoServer
-  puts 'running echo server on 8081'
 end
