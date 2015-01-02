30a8883d9e2b7858dbf23d9c8fc4cea7340f0367
Will Sewell <me@willsewell.name>
2015-01-02 18:35:07 +0000

    Multiple actions are now supported.
    Each websocket message now has an associated action which is what
    determines the handler to execute. The new functionality that has been
    added to this is support for loading a user's previous clicked countries
    when they first connect.

--- main.rb
+++ main.rb
@@ -8,6 +8,69 @@ require 'em-hiredis'
 require 'em-websocket'
 require 'json'
 
+def update_country ws, redis, user, country, isSelected
+    if isSelected
+      redis.sadd(user, country).callback {
+        redis.smembers(user).callback { |get_res|
+          puts ">>> cached, set: #{get_res}"
+          send_rankings ws, redis, user
+        }
+      }
+    else
+      redis.srem(user, country).callback {
+        redis.smembers(user).callback { |get_res|
+          puts ">>> removed, set: #{get_res}"
+          send_rankings ws, redis, user
+        }
+      }
+    end
+end
+
+def send_rankings ws, redis, user
+  country_rankings = {}
+  redis.keys("*").callback { |keys|
+    num_keys_left = keys.length - 1
+    keys.each do |k|
+      if k != user.to_s
+        redis.sinter(user, k).callback { |inter|
+          redis.sunion(user, k).callback { |union|
+            similarity = inter.length.to_f / union.length
+            redis.smembers(k).callback { |other_user_countries|
+              other_user_countries.each do |country|
+                found = false
+                country_rankings.each do |other_country, rank|
+                  if other_country == country
+                    rank += similarity
+                    found = true
+                  end
+                end
+                if !found
+                  country_rankings[country] = similarity
+                end
+              end
+              num_keys_left -= 1
+              if num_keys_left < 1
+                response = {
+                  action: "country_clicked",
+                  rankings: country_rankings
+                }
+                ws.send(response.to_json)
+              end
+            }
+          }
+        }
+      end
+    end
+  }
+end
+
+def get_selected ws, redis, user
+  redis.smembers(user).callback { |members|
+    response = { action: "get_selected", selected: members }
+    ws.send(response.to_json)
+  }
+end
+
 EM::run do
   redis = EM::Hiredis.connect
   EM::WebSocket.run(:host => "127.0.0.1", :port => 8081) do |ws|
@@ -18,55 +81,17 @@ EM::run do
     ws.onclose { puts "Connection closed" }
 
     ws.onmessage { |msg|
+      puts msg
       begin
         data = JSON.parse msg
-        if data["isSelected"]
-          redis.sadd(data["user"], data["country"]).callback {
-            redis.smembers(data["user"]).callback { |get_res|
-              puts ">>> cached, set: #{get_res}"
-            }
-          }
-        else
-          redis.srem(data["user"], data["country"]).callback {
-            redis.smembers(data["user"]).callback { |get_res|
-              puts ">>> removed, set: #{get_res}"
-            }
-          }
+        if data["action"] == "country_clicked"
+          update_country ws, redis, data["user"], data["country"], data["isSelected"]
+        elsif data["action"] == "get_selected"
+          get_selected ws, redis, data["user"]
         end
       rescue JSON::ParserError
-        ws.send "failed to parse JSON!"
+        puts "failed to parse JSON!"
       end
-      country_rankings = {}
-      redis.keys("*").callback { |keys|
-        num_keys_left = keys.length
-        keys.each do |k|
-          if k != data["user"]
-            redis.sinter(data["user"], k).callback { |inter|
-              redis.sunion(data["user"], k).callback { |union|
-                similarity = inter.length.to_f / union.length
-                redis.smembers(k).callback { |other_user_countries|
-                  other_user_countries.each do |country|
-                    found = false
-                    country_rankings.each do |other_country, rank|
-                      if other_country == country
-                        rank += similarity
-                        found = true
-                      end
-                    end
-                    if !found
-                      country_rankings[country] = similarity
-                    end
-                  end
-                  num_keys_left -= 1
-                  if num_keys_left > 0
-                    ws.send(country_rankings.to_json)
-                  end
-                }
-              }
-            }
-          end
-        end
-      }
     }
   end
 end
