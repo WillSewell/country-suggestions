6259ba3fe1071e313d99e0829786237008f5c96e
Will Sewell <me@willsewell.name>
2015-01-12 21:55:13 +0000

    Rather than manually computing similarity and checking it's complete, now using EM's Iterator to handle this.
    Also made the computing of rankings more functional, but reducing the similarities.

--- main.rb
+++ main.rb
@@ -29,62 +29,67 @@ end
 
 # Compute rankings based on similarities to all other users
 def send_rankings ws, redis, user, user_countries
-  country_rankings = {}
-  max_ranking = 0
-  # First get and loop through other users selections
+  # First get and loop through all other user's selections
   redis.keys("*").callback do |keys|
-    num_keys_left = keys.length - 1
-    keys.each do |k|
-      if k != user.to_s
-        # For each other user, compute the similarity using the Jaccard index
-        redis.smembers(k).callback do |other_countries|
-          inter = user_countries & other_countries
-          union = user_countries | other_countries
-          similarity = inter.length.to_f / union.length
-          # For each country of the other user, increase the rank of
-          # that country based on the similarity
-          puts k + other_countries.inspect
-          puts k + inter.inspect
-          puts k + union.inspect
-          puts k + similarity.inspect
+    EM::Iterator.new(keys, keys.length).map(
+      proc do |k, iter|
+        if k == user.to_s
+          # We don't care about the similarity of the current user to themselves
+          # Just return a dummy value with similarity 0 so it is not counted
+          iter.return [0, []]
+        else
+          # For each other user, compute the similarity using the Jaccard index
+          redis.smembers(k).callback do |other_countries|
+            inter = user_countries & other_countries
+            union = user_countries | other_countries
+            similarity = inter.length.to_f / union.length
+            iter.return [similarity, other_countries]
+          end
+        end
+      end,
+      proc do |results|
+        # For each country of the other user, increase the rank of
+        # that country based on the similarity
+        rankings_and_max = results.reduce([{}, 0]) do |acc, result|
+          similarity = result[0]
+          other_user_countries = result[1]
+          country_rankings = acc[0]
+          max_ranking = acc[1]
           if similarity > 0
-            other_countries.each do |other_country|
-              unless user_countries.include? other_country
+            other_user_countries.each do |other_user_country|
+              # Don't bother suggesting countries they have already clicked
+              unless user_countries.include? other_user_country
                 found = false
                 country_rankings.each do |ranked_country, rank|
-                  if ranked_country == other_country
+                  if ranked_country == other_user_country
                     # No need to suggest countries already selected
-                    country_rankings[other_country] += similarity
+                    country_rankings[other_user_country] += similarity
                     found = true
                   end
                 end
                 unless found
-                  country_rankings[other_country] = similarity
+                  country_rankings[other_user_country] = similarity
                 end
-                if country_rankings[other_country] > max_ranking
-                  max_ranking = country_rankings[other_country]
+                if country_rankings[other_user_country] > max_ranking
+                  max_ranking = country_rankings[other_user_country]
                 end
               end
             end
           end
-          puts k + " " + country_rankings.inspect
-          # If all other users of been looked at, send the similarities
-          # TODO: need to think of race conditions here with num_keys_left
-          num_keys_left -= 1
-          if num_keys_left < 1
-            # normalise
-            country_rankings.each do |country, rank|
-              country_rankings[country] = rank / max_ranking # normalize
-            end
-            response = {
-              action: "country_clicked",
-              rankings: country_rankings,
-            }
-            ws.send(response.to_json)
-          end
+          [country_rankings, max_ranking]
         end
+        rankings = rankings_and_max[0]
+        max_ranking = rankings_and_max[1]
+        normalised_rankings = Hash[rankings.map do |country, rank|
+          [country, rank / max_ranking ]
+        end]
+        response = {
+          action: "country_clicked",
+          rankings: normalised_rankings,
+        }
+        ws.send(response.to_json)
       end
-    end
+    )
   end
 end
 
